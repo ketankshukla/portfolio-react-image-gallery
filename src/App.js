df7486a6a77a4ebd56798a43e399ad47a2316d11
Ketan Shukla <ketankshukla@gmail.com>
2021-01-22 20:15:51 -0800

    finally got the settings right

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <div className="App">
-      <h1 className="text-6xl">Hello World</h1>
+      <h1 className="text-5xl underline text-center m-5 ml-10 bg-red-100 p-5">Image Gallery</h1>
     </div>
   );
 };
